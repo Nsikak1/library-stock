00b9b493527aabb71bd8355b80c88af94f9af8f0
Christopher Nsikak <christophernsikak1@gmail.com>
2026-01-19 13:07:50 +0100

    Testing id fix

--- src/routes/db.ts
+++ src/routes/db.ts
@@ -8,7 +8,7 @@ interface Friend {
 }
 
 interface ISpreadsheetData {
-    id: number,
+    // id: number,
     accession: string,
     book_name: string,
     isbn: string,
@@ -24,7 +24,6 @@ interface ISpreadsheetData {
 
 // Define the target keys in order
 export const spreadsheetKeys: (keyof ISpreadsheetData)[] = [
-  'id',
   'accession',
   'book_name',
   'isbn',
@@ -39,15 +38,15 @@ export const spreadsheetKeys: (keyof ISpreadsheetData)[] = [
 
 const db = new Dexie("SpreadsheetDatabase") as Dexie & {
   spreadsheets: EntityTable<
-    ISpreadsheetData,
-    "id" // primary key "id" (for the typings only)
+    ISpreadsheetData
+    //"id"  primary key "id" (for the typings only)
 
   >
 }
 
 // Schema declaration:
 db.version(1).stores({
-  spreadsheets: "++id, accession, book_name, isbn, book_type, author, published, num_of_pages, image_links, language", // primary key "id" (for the runtime!)
+  spreadsheets: " accession, book_name, isbn, book_type, author, published, num_of_pages, image_links, language", // primary key "id" (for the runtime!)
 })
 
 export type { ISpreadsheetData }
